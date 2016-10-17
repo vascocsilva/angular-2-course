a2635aa016ade921c18f1a3ac7e8b6a30c281602
Vasco Silva <vasco.s.rebolo@gmail.com>
2016-10-17 14:21:11 +0100

    Add more into lifecycle component

--- src/app/databinding/event-binding.component.ts
+++ src/app/databinding/event-binding.component.ts
@@ -10,6 +10,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class EventBindingComponent {
   @Output('clickable') clicked = new EventEmitter<string>(); // use string, typed thing (could be object...)
   // @Outpu makes the event emition available outside the component
+  // 'clickable' is the name of event recognizable outside the component (not usually done this way)
 
   onClicked() {
     this.clicked.emit('it works!');
